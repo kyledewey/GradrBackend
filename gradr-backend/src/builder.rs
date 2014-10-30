a35f4ecd7af9127f18d4021c8c28770e04feac47
Kyle Dewey <kyledewey@cs.ucsb.edu>
2014-10-30 12:33:37 -0700

    Added (untested) code for doing testing.

--- gradr-backend/src/builder.rs
+++ gradr-backend/src/builder.rs
@@ -8,8 +8,10 @@
 // ...where each test result is on its own line.  If multiple tests
 // have the same name, then only the last test is recorded.
 
-use std::io::{IoResult, IoError, OtherIoError};
+use std::io::{BufferedReader, RefReader, IoResult, IoError, OtherIoError};
+use std::io::pipe::PipeStream;
 use std::io::process::{Command, Process, ExitStatus, ExitSignal};
+use std::collections::HashMap;
 
 /// Peforms builds and testing via abstract requests.
 trait Builder<A : Tester> {
@@ -52,7 +54,83 @@ trait Builder<A : Tester> {
     }
 } // trait Builder
 
-trait Tester {}
+enum TestResult {
+    Pass,
+    Fail
+}
+
+struct ProcessReader {
+    p: Process
+}
+
+impl ProcessReader {
+    fn new(cmd: &Command) -> IoResult<ProcessReader> {
+        cmd.spawn().map(|p| ProcessReader { p: p })
+    }
+
+    fn output_reader<'a>(&'a mut self) -> BufferedReader<RefReader<'a, PipeStream>> {
+        // unwrap should be ok - the documentation says `Some` is the default,
+        // and we are not messing with any of the defaults
+        BufferedReader::new(
+            Reader::by_ref(
+                self.p.stdout.as_mut().unwrap()))
+    }
+}
+
+trait Tester {
+    fn parse_test_result(line: &str) -> IoResult<TestResult> {
+        match line {
+            "PASS" => Ok(Pass),
+            "FAIL" => Ok(Fail),
+            _ => Err(
+                IoError {
+                    kind: OtherIoError,
+                    desc: "Malformed test result",
+                    detail: Some(line.to_string())
+                })
+        }
+    }
+
+    fn parse_line(line: &str) -> IoResult<(String, TestResult)> {
+        let results: Vec<&str> = line.split_str(":").collect();
+        if results.len() == 2 {
+            Tester::parse_test_result(results[1]).map(|res| {
+                (results[0].to_string(), res)
+            })
+        } else {
+            Err(
+                IoError {
+                    kind: OtherIoError,
+                    desc: "Malformed test string",
+                    detail: Some(line.to_string())
+                })
+        }
+    }
+
+    fn do_testing(&self) -> IoResult<HashMap<String, TestResult>> {
+        match ProcessReader::new(&*Command::new("make").arg("test")) {
+            Ok(mut reader) => {
+                let mut map = HashMap::new();
+                for op_line in reader.output_reader().lines() {
+                    match op_line {
+                        Ok(line) => {
+                            match Tester::parse_line(line.as_slice()) {
+                                Ok((k, v)) => {
+                                    map.insert(k, v);
+                                }
+                                Err(s) => { return Err(s); }
+                            }
+                        }
+                        Err(s) => { return Err(s); }
+                    }
+                }
+                Ok(map)
+                
+            }
+            Err(s) => { return Err(s); }
+        }
+    }
+}
 
 /// Encapsulates a request to perform a build and begin test execution.
 /// In the future, this may include information for spinning up Docker
