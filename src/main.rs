9eff77496451e072c469c18352454bce85abf83e
Kyle Dewey <kyledewey@cs.ucsb.edu>
2014-11-28 14:54:48 -0800

    Testing (uncompiled)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+extern crate hyper;
 extern crate gradr_backend;
+
+use hyper::Ipv4Addr;
+use gradr_backend::notification_listener::GitHubServer;
 
 #[cfg(not(test))]
 fn main() {
-    println!("Hello, world!");
+    let server = GitHubServer::new(Ipv4Addr(0, 0, 0, 0), 1337);
+    let running_server = server.event_loop().unwrap();
+    loop {
+        println!("{}", running_server.get_notification());
+    }
 }
