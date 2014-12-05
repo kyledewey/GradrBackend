3bf6371000bb94255926cfa17dbfdd4537ae9e84
Kyle Dewey <kyledewey@cs.ucsb.edu>
2014-12-05 09:58:16 -0800

    Added separate executable for listening for GitHub notifications.

---
+++ gradr_notifications/src/main.rs
@@ -0,0 +1,22 @@
+extern crate hyper;
+extern crate libgradr;
+
+use hyper::Ipv4Addr;
+
+use libgradr::database::postgres_db::PostgresDatabase;
+use libgradr::notification_listener::{GitHubServer, NotificationSource};
+
+static PORT: u16 = 1337;
+
+#[cfg(not(test))]
+fn main() {
+    let db = PostgresDatabase::new(
+        "postgres://jroesch@localhost/gradr-production").unwrap();
+
+    let server = GitHubServer::new(Ipv4Addr(0, 0, 0, 0), PORT);
+    let running_server = server.event_loop().unwrap();
+
+    loop {
+        running_server.notification_event_loop_step(&db);
+    }
+}
