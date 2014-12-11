d3094ef80b1d096d45d9d969bd4f542916dab501
Kyle Dewey <kyledewey@cs.ucsb.edu>
2014-12-11 21:41:52 +0000

    Have everything working on AWS.

---
+++ gradr_worker/build_test/README.md
@@ -0,0 +1,2 @@
+This README is here just so this directory sticks around in git.
+It directory is needed to run integration tests.
--- gradr_worker/src/main.rs
+++ gradr_worker/src/main.rs
@@ -5,8 +5,7 @@ use libgradr::worker::worker_loop_step;
 
 #[cfg(not(test))]
 fn main() {
-    let db = PostgresDatabase::new(
-        "postgres://jroesch@localhost/gradr-production").unwrap();
+    let db = PostgresDatabase::new_development().unwrap();
     
     loop {
         worker_loop_step(&db);
